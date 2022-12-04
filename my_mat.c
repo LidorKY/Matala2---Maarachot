ce58b36a8e876cbad670822ec79df50d82aadff1
LidorKY <118388214+LidorKY@users.noreply.github.com>
2022-12-04 18:24:03 +0200

    Add files via upload

--- my_mat.c
+++ my_mat.c
@@ -10,7 +10,8 @@ int initializingMat (int mat [10][10]){
 int num;
 for(int i = 0; i < 10; i++){
     for(int j = 0; j < 10; j++){
-        mat[i][j] = scanf("%d",&num);
+        scanf("%d",&num);
+        mat[i][j] = num;
     }
 }
 return 0;
